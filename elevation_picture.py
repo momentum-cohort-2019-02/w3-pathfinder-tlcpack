01b1bb74467c3f034aaa967719907d330d897039
Taylor Cooke <taylorcooke@gmail.com>
2019-03-01 11:12:16 -0500

    added classes, trying to draw map

---
+++ elevation_picture.py
@@ -0,0 +1,68 @@
+from PIL import ImageColor, ImageDraw, Image
+# # class notes: class ElevationMap:
+
+#     def __init__(self, filename):
+#         self.elevations = []
+#         with open(filename) as file:
+#             for line in file:
+#                 self.elevations.append([int(e) for e in line.split()])
+
+
+#         # max_elevations = []
+#         # for row in self.elevations:
+#         #     max_elevations.append(max(row))
+#         # self.max_elevations = max(max_elevations)
+
+#         self.max_elevation = max(max(row) for row in self.elevations)
+#         self.min_elevation = min(min(row) for row in self.elevations)
+#     def ele_print(self, max_elevation, min_elevation):
+#         print(min_elevation)
+#     def get_elevation(self, x, y):
+#         return self.elevations[y][x]
+
+#     def get_intensity(self, x, y):
+#         return (self.get_elevation(x, y) - self.min_elevation) / (self.max_elevation - self.min_elevation) * 255
+
+# ❸ >>> for x in range(100):
+#            for y in range(50):
+# ❹             im.putpixel((x, y), (210, 210, 210))
+
+class Map:
+
+    def __init__(self, filename):
+        self.elevations = []
+        with open(filename) as file:
+            for line in file:
+                self.elevations.append([int(elev) for elev in line.split(" ")])
+        
+        # finding highest and lowest points on map
+        self.max_overall_elev = max(max(row) for row in self.elevations)
+        self.min_overall_elev = min(min(row) for row in self.elevations)
+        print(self.max_overall_elev)
+        print(self.min_overall_elev)
+
+    def get_elevation(self, x, y):
+        """flipping the coordinates so they're easier to read"""
+        return self.elevations[y][x]
+
+    def get_intensity(self, x, y):
+        """determine intensity of pixel for map"""
+        return int((self.get_elevation(x, y) - self.min_overall_elev) / (self.max_overall_elev - self.min_overall_elev) * 255)
+
+class DrawMap:
+    
+
+    def __init__(self, map):
+        self.map = map
+        self.picture = Image.new('RGBA', (len(self.map.elevations[0], len(self.map.elevations)))
+
+    # def draw(self):
+    #     pass
+
+
+
+if __name__ == "__main__":
+
+    
+    small = Map(elevation_small.txt)
+    
